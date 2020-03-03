9a54edd1b6ca520b9e0dd14fc434d7d9dceb9f1f
picpoint <picpoint@yandex.ru>
2020-03-03 23:03:53 +0300

    work with slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,4 +1,35 @@
-const slideblock = document.querySelector('.slideblock');
-let arr = ['01.jpg', '02.jpg', '03.jpg'];
+const slblock = document.querySelector('.slblock__slider');
+const wdthScreen = document.documentElement.clientWidth;
+let mass = ['01.jpg', '02.jpg', '03.jpg'];
 
-console.log(slideblock);
+console.log(`width screen - ${wdthScreen}`);
+
+
+class ShowSlide {
+  constructor(blockslider, arr) {
+    this.blockslider = blockslider;
+    this.arr = arr;
+  } 
+
+  show() {
+    let offset = 0;
+    
+    setTimeout(() => {
+      this.blockslider.firstElementChild.style.position = 'relative';      
+      for(let i = 0; i < this.arr.length; i++) {
+        setInterval(() => {
+          this.arr[i].style.position
+        }, 1500);
+      }
+    }, 3000);
+    
+    
+    
+    this.blockslider.firstElementChild.setAttribute('src', `/pict/${this.arr[0]}`);    
+  }
+
+}
+
+
+let slider = new ShowSlide(slblock, mass);
+slider.show();
